0e6a1890bf12ff8263eade77dbf0229f2166a70b
miu <miu@miu-no-MacBook.local>
2015-08-04 00:02:45 +0900

    user name

--- WebContent/app/index.js
+++ WebContent/app/index.js
@@ -6,11 +6,13 @@ require([
 	// divLoading削除 (ここ？)
 	
 	// ログイン画面構築 (from login.html)
-	requre([
+	require([
 	        "dojo/dom-construct",
 	        "dojo/text!app/login.html",
 	        "dojo/_base/window"
 	], function (domConstruct, html, win) {
 		domConstruct.place(domConstruct.toDom(html), win.body());
+		
+		domConstruct.destroy('divLoading');
 	});
 });
