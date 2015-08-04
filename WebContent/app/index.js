2072104757c9f2472d1ba4f53802a68985106d1f
miu <armfulofmelons@gmail.com>
2015-08-04 12:38:20 +0900

    change

--- WebContent/app/index.js
+++ WebContent/app/index.js
@@ -1,16 +1,12 @@
 require([
-], function () {
+	"dojo/dom-construct",
+	"dojo/text!app/login.html",
+	"dojo/_base/window"
+], function (domConstruct, html, win) {
 	// ブラウザーチェックとかやる？
-	// 画面ブロック (ログイン画面構築を見せないようにする)
+	// 画面ブロック (ログイン画面構築を見せないようにする) → dojo overlay
 	
 	// ログイン画面構築 (from login.html)
-	require([
-	        "dojo/dom-construct",
-	        "dojo/text!app/login.html",
-	        "dojo/_base/window"
-	], function (domConstruct, html, win) {
-		domConstruct.place(domConstruct.toDom(html), win.body());
-		
-		domConstruct.destroy('divLoading');
-	});
+	domConstruct.place(domConstruct.toDom(html), win.body());
+	domConstruct.destroy('divLoading');
 });
